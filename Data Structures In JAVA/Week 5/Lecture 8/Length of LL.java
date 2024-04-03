7e0a75ea9a48cfa79f221e1aff4b3d6b75d4a6cf
Shruti Puskar <146525433+shrutipuskar@users.noreply.github.com>
2024-04-03 23:18:35 +0530

    Create Length of LL.java

---
+++ Data Structures In JAVA/Week 5/Lecture 8/Length of LL.java
@@ -0,0 +1,83 @@
+/*Problem statement
+Given the head of a singly linked list of integers, find and return its length.
+
+
+Example:
+Sample Linked List
+
+The length of the list is 4. Hence we return 4.
+
+Note:
+Exercise caution when dealing with edge cases, such as when the head is NULL. Failing to handle these edge cases appropriately may result in a runtime error in your code.
+
+
+Detailed explanation ( Input/output format, Notes, Images )
+Sample Input 1 :
+3 4 5 2 6 1 9 -1
+
+
+Sample Output 1 :
+7
+
+
+Explanation of sample input 1 :
+The number of nodes in the given linked list is 7.
+Hence we return 7.
+
+
+Sample Input 2 :
+10 76 39 -3 2 9 -23 9 -1
+
+Sample Output 2 :
+8
+
+
+Expected Time Complexity:
+Try to do this in O(n).
+
+
+ Constraints :
+0 <= N <= 10^5
+Time Limit: 1 sec*/
+
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+    
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+    
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+    
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ }
+
+ *****************************************************************/
+
+public class Solution {
+    public static int length(Node head){
+        int count=0;
+        Node temp=head;
+        while(temp.next!=null)
+        {
+            count++;
+            temp=temp.next;
+        }
+        return count+1;
+    }
+}
