febe577437a92fd9c5aa50ad384896436af18d79
Shruti Puskar <146525433+shrutipuskar@users.noreply.github.com>
2024-04-27 18:01:09 +0530

    Create Nodes Without Sibling.java

---
+++ Data Structures In JAVA/Week 7/Lecture 13: Binary Trees/Assignment/Nodes Without Sibling.java
@@ -0,0 +1,59 @@
+/*Problem statement
+For a given Binary Tree of type integer, print all the nodes without any siblings.
+
+Example Input :
+1 4 5 6 -1 -1 7 20 30 80 90 -1 -1 -1 -1 -1 -1 -1 -1
+Explanation:
+The input tree when represented in a two-dimensional plane, it would look like this:     
+alt txt
+
+In respect to the root, node data in the left subtree that satisfy the condition of not having a sibling would be 6, taken in a top-down sequence. Similarly, for the right subtree, 7 is the node data without any sibling.
+
+Since we print the siblings in the left-subtree first and then the siblings from the right subtree, taken in a top-down fashion, we print 6 7.
+Example Output:
+6 7  
+Detailed explanation ( Input/output format, Notes, Images )
+ Constraints:
+1 <= N <= 10^5
+Where N is the total number of nodes in the binary tree.
+
+Time Limit: 1 second
+Sample Input 1:
+5 6 10 2 3 -1 -1 -1 -1 -1 9 -1 -1
+Sample Output 1:
+9    
+
+
+*/
+/*
+	
+	Following is the structure used to represent the Binary Tree Node
+
+	class BinaryTreeNode<T> {
+		T data;
+		BinaryTreeNode<T> left;
+		BinaryTreeNode<T> right;
+
+		public BinaryTreeNode(T data) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+	}
+
+*/
+
+public class Solution {
+
+	public static void printNodesWithoutSibling(BinaryTreeNode<Integer> root) {
+		if(root==null)
+			return;
+		if(root.left!=null&&root.right==null)
+			System.out.print(root.left.data+" ");
+		else if(root.right!=null&&root.left==null)
+			System.out.print(root.right.data+" ");
+		printNodesWithoutSibling(root.left);
+		printNodesWithoutSibling(root.right);
+	}
+
+}
