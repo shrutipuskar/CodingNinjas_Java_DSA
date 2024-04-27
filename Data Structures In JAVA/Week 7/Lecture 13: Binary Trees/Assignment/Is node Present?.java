be91c044e71316dc8c692b6732078ceccfdf299e
Shruti Puskar <146525433+shrutipuskar@users.noreply.github.com>
2024-04-27 18:00:19 +0530

    Create Is node Present?.java

---
+++ Data Structures In JAVA/Week 7/Lecture 13: Binary Trees/Assignment/Is node Present?.java
@@ -0,0 +1,52 @@
+/*Problem statement
+For a given Binary Tree of type integer and a number X, find whether a node exists in the tree with data X or not.
+
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints:
+1 <= N <= 10^5
+
+Where N is the total number of nodes in the binary tree.
+
+Time Limit: 1 sec.
+Sample Input 1:
+8 3 10 1 6 -1 14 -1 -1 4 7 13 -1 -1 -1 -1 -1 -1 -1
+7
+Sample Output 1:
+true
+Explanation For Output 1:
+Clearly, we can see that 7 is present in the tree. So, the output will be true.
+Sample Input 2:
+2 3 4 -1 -1 -1 -1
+10
+Sample Output 2:
+false*/
+
+/*
+	
+	Following is the structure used to represent the Binary Tree Node
+
+	class BinaryTreeNode<T> {
+		T data;
+		BinaryTreeNode<T> left;
+		BinaryTreeNode<T> right;
+
+		public BinaryTreeNode(T data) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+	}
+
+*/
+
+public class Solution {
+
+	public static boolean isNodePresent(BinaryTreeNode<Integer> root, int x) {
+	    if(root==null)
+			return false;
+		if(root.data==x)
+			return true;
+		return (isNodePresent(root.left, x)||isNodePresent(root.right, x));
+	}
+
+}
